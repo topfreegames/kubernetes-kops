2c5bf1b1032de46d3be5643a57208f56b3605231
liranp <liran.polak@gmail.com>
2019-04-17 12:39:36 +0300

    feat(spotinst): new instance group type: ocean

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -572,7 +572,7 @@ func (b *BootstrapChannelBuilder) buildAddons() *channelsapi.Addons {
 
 	if featureflag.Spotinst.Enabled() {
 		key := "spotinst-kubernetes-cluster-controller.addons.k8s.io"
-		version := "1.0.18"
+		version := "1.0.39"
 
 		{
 			id := "v1.8.0"
